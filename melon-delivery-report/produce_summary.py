d695b7c9304326f46d96bb510e9ee68f5c38433d
Laura Docherty <laurahdocherty@gmail.com>
2020-07-10 15:35:03 +0000

    New function created.

--- melon-delivery-report/produce_summary.py
+++ melon-delivery-report/produce_summary.py
@@ -1,45 +1,23 @@
 
+def melon_count(day, file):
 
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+    print("Day", day)
+    the_file = open(file)
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
 
+        print("Delivered {} {}s for total of ${}".format(
+            count, melon, amount))
+    the_file.close()
 
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+melon_count(1, "um-deliveries-20140519.txt")
+melon_count(2, "um-deliveries-20140520.txt")
+melon_count(3, "um-deliveries-20140521.txt")
+    
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
